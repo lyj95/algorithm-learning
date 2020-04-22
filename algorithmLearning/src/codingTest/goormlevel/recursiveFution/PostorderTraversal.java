8471073a7b1f0c91b4b7e44904994016a947ad40
lyj95 <dpwl031@naver.com>
2020-04-22 16:28:12 +0900

    [goorm] Postorder Traversal

---
+++ algorithmLearning/src/codingTest/goormlevel/recursiveFution/PostorderTraversal.java
@@ -0,0 +1,14 @@
+package codingTest.goormlevel.recursiveFution;
+
+public class PostorderTraversal {
+	
+}
+class Main {
+	public static void main(String[] args) throws Exception {
+		int[] arr = {6,3,9,1,5,7,11};
+		
+		for(int i=0 ; i<arr.length ; i++){
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
