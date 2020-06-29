5712743617bb133a0497f95df14b563bd18c6dc1
lyj95 <dpwl031@naver.com>
2020-06-29 17:04:36 +0900

    DP 문제

--- algorithmLearning/src/codingTest/backjoon/BridgeLaying.java
+++ algorithmLearning/src/codingTest/backjoon/BridgeLaying.java
@@ -11,33 +11,34 @@ public class BridgeLaying {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		int[][] site = new int[T][2];
+		int T = Integer.parseInt(br.readLine());			// 테스트케이스의 개수
+		int[][] site = new int[T][2];						// 각 테스트 케이스 서쪽과 동쪽에 있는 사이트 개수
 		for (int i = 0; i < T; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			site[i][0] = Integer.parseInt(st.nextToken());
-			site[i][1] = Integer.parseInt(st.nextToken());
+			site[i][0] = Integer.parseInt(st.nextToken());		// i번째 테스트케이스의 서쪽에 있는 사이트 개수 N
+			site[i][1] = Integer.parseInt(st.nextToken());		// i번째 테스트케이스의 동쪽에 있는 사이트 개수 M
 		}
 
 		for (int[] s : site) {
-			long res;
-			if (s[1] == s[0]) {
+			long res;	//결과
+			if (s[1] == s[0]) {			// 동서쪽에 있는 사이트의 개수가 같으면 다리를 놓는 경우의 수는 1개 뿐
 				res = 1;
 			} else {
-				int x = s[1] - s[0] >= s[0] ?  s[1] - s[0] :s[0];
-				int y = s[1] - s[0] >= s[0] ?  s[0] :s[1] - s[0];
-				long z = s[1];
+				int x = s[1] - s[0] >= s[0] ?  s[1] - s[0] :s[0];		// M-N or N 중 큰 값을 x
+				int y = s[1] - s[0] >= s[0] ?  s[0] :s[1] - s[0];		// M-N or N 중 작은 값을 y
 				
+				long z = s[1];						
 				for (int i = s[1] - 1; i > x; i--) {
-					z *= i;
+					z *= i;								// M * M-1 * M-2 * ...* x-1 
 				}
-				res = z / factorial(y);
+				res = z / factorial(y);					// M * M-1 * M-2 * ...* x-1 / y!
 			}
 			System.out.println(res);
 		}
 	}
 
-	public static long factorial(int n) {
+	// n!을 구하는 함수
+	public static long factorial(int n) {	// 처음에 펙토리얼로 할라고 만들고	
 		if (n == 0)
 			return 1;
 		if (n == 1)
--- algorithmLearning/src/codingTest/backjoon/Fibonacci.java
+++ algorithmLearning/src/codingTest/backjoon/Fibonacci.java
@@ -7,26 +7,26 @@ import java.io.InputStreamReader;
 public class Fibonacci {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] testCase = new int[N];
-		for(int i=0; i<N; i++) {
+		int T = Integer.parseInt(br.readLine());	// testCase의 개수			
+		int[] testCase = new int[T];				// 각 테스트 케이스의 값의 배열
+		for(int i=0; i<T; i++) {
 			testCase[i] = Integer.parseInt(br.readLine());
 		}
 		
-		int[] zero = new int[41];
+		int[] zero = new int[41];		
 		int[] one = new int[41];
 		
-		zero[0] = 1;
-		zero[1] = 0;
-		one[0] = 0;
-		one[1] = 1;
+		zero[0] = 1;						// 0일 때 0의 개수는 1개
+		zero[1] = 0;						// 1일 때 0의 개수는 0개
+		one[0] = 0;							// 0일 때 1의 개수는 0개
+		one[1] = 1;							// 1일 때 1의 개수는 1개
 		
-		for(int i=2; i<=40; i++) {
+		for(int i=2; i<=40; i++) {			// 2~40까지의 자연수의 0(zero)과 1(one)이 출력되는 횟수를 저장
 			zero[i] = zero[i-1] + zero[i-2];
 			one[i] = one[i-1] + one[i-2];
 		}
 		
-		for(int t : testCase) {
+		for(int t : testCase) {				// 각 테스트 케이스의 값의 0, 1이 출력되는 횟수 출력
 			System.out.println(zero[t] +" "+ one[t]);
 		}
 	}
