1cd4c5ac5a405162dedd46f5a749033295b55dbb
lyj95 <dpwl031@naver.com>
2020-07-22 14:18:17 +0900

    백준 1026번 : 보물

---
+++ algorithmLearning/src/codingTest/backjoon/Treasure.java
@@ -0,0 +1,18 @@
+package codingTest.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 1026번 : 보물
+public class Treasure {
+	public static void main(String args[]) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			
+		}
+	}
+}
