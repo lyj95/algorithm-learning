4c8005fa83b196d2ba4c88f597e1cfa45718a8db
lyj95 <dpwl031@naver.com>
2020-09-08 17:00:17 +0900

    [프로그래머스] 정렬 알고리즘 문제

--- algorithmLearning/src/codingTest/programmers/sort/HIndex.java
+++ algorithmLearning/src/codingTest/programmers/sort/HIndex.java
@@ -6,35 +6,52 @@ public class HIndex {
 
 	public static void main(String[] args) {
 		HIndex h = new HIndex();
-		int[] ci = {3, 0, 6, 1, 5};
+		int[] ci = { 3, 0, 6, 1, 5 };
 		System.out.println(h.solution(ci));
 	}
-	
+
 	public int solution(int[] citations) {
-        
-        Arrays.sort(citations);
-        
-        int hIndex = citations.length;
-        
-        if(hIndex == 0) {
-        	return 0;
-        }else {
-        	while(true) {
-        		int up = 0, down = 0;
-        		for(int i=citations.length-1; i>=0; i--) {
-        			if(hIndex <= citations[i]) {
-        				up++;
-        			}else {
-        				down++;
-        			}
-        		
-        		}
-        		if(up >= hIndex || down >= hIndex) {
-        			return hIndex; 
-        		}
-        		hIndex--;	
-        	}
-        }
-    }
+		int answer = 0;
+		Arrays.sort(citations);
+
+		for (int i = 0; i < citations.length; i++) {
+			int h = citations.length - i;
+			System.out.println("h : "+h);
+			
+			if (citations[i] >= h) {
+				answer = h;
+				break;
+			}
+		}
+
+		return answer;
+	}
+
+//	public int solution(int[] citations) {
+//        
+//        Arrays.sort(citations);
+//        
+//        int hIndex = citations.length;
+//        
+//        if(hIndex == 0) {
+//        	return 0;
+//        }else {
+//        	while(true) {
+//        		int up = 0, down = 0;
+//        		for(int i=citations.length-1; i>=0; i--) {
+//        			if(hIndex <= citations[i]) {
+//        				up++;
+//        			}else {
+//        				down++;
+//        			}
+//        		
+//        		}
+//        		if(up >= hIndex || down >= hIndex) {
+//        			return hIndex; 
+//        		}
+//        		hIndex--;	
+//        	}
+//        }
+//    }
 
 }
