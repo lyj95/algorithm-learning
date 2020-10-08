4a13fd873190e47b378217e6f18bda27fcc36932
lyj95 <dpwl031@naver.com>
2020-10-08 15:52:43 +0900

    [프로그래머스] 코드챌린지1-2

--- algorithmLearning/src/codingTest/programmers/codeChallenge01/Solution2.java
+++ algorithmLearning/src/codingTest/programmers/codeChallenge01/Solution2.java
@@ -4,25 +4,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution2 {
-	private List<Integer> list = new ArrayList<Integer>();
-	
-	private int tmp=1;
-	public int[] solution(int n) {
-		List<int[]> list = new ArrayList<int[]>()
-		list
-		int[] answer = list.si;
-		
-		for(int i=0; i<n; i++) {
-			
-		}
-		
-        return answer;
-    }
-	
-	public int snail(int n) {
-		for(int i=0; i<n; i++) {
-			list.add(i);
-		}
-		return snail(n-1);
+
+	public static void main(String args[]) {
+		Solution2 s = new Solution2();
+		s.solution(5);
 	}
+
+	public int[] solution(int n) {
+		int res = 0;
+		int direction = 1;
+		int x = -1, y = 0;
+		int[][] arr = new int[n][n];
+		
+		while(n >0) {
+			if (direction == 1) {
+				for (int i = 0; i < n; i++) {
+					arr[++x][y] = ++res;
+				}
+				direction = 0;
+				n--;
+			} else if (direction == 0) {
+				for (int i = 0; i < n; i++) {
+					arr[x][++y] = ++res;
+				}
+				direction = -1;
+				n--;
+				
+			} else if (direction == -1) {
+				for (int i = 0; i < n; i++) {
+					arr[--x][--y] = ++res;
+				}
+				direction = 1;
+				n--;
+			}
+		}
+		
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+//				System.out.print("값: ");
+//				System.out.print(arr[i][j]);
+//				System.out.println();
+				if(arr[i][j]!=0) {
+					list.add(arr[i][j]);
+				}
+			}
+		}
+
+		int[] answer = new int[list.size()];
+		int index = 0;
+		for(int a : list) {
+			answer[index++] = a;
+		}
+		return answer;
+	}
+
 }
