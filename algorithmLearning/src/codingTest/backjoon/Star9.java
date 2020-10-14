d693e89096fe84ab491ceee11184114ee81ecaa3
lyj95 <dpwl031@naver.com>
2020-10-14 16:04:00 +0900

    [BOJ] 별 찍기 9

---
+++ algorithmLearning/src/codingTest/backjoon/Star9.java
@@ -0,0 +1,33 @@
+package codingTest.backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 2446번 : 별 찍기 - 9
+public class Star9 {
+
+	public static void main(String[] args) throws Exception  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<2*N-1-(2*i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1; i<N; i++) {
+			for(int j=N-1-i; j>0 ;j--) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<2*i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
